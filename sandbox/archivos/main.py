23d399c98014e28ac53ed843afb7228f379b9eb9
Salvador HM <salvadorhm@gmail.com>
2023-10-09 10:45:37 -0600

    UPDATE archivos rota imagen

--- sandbox/archivos/main.py
+++ sandbox/archivos/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from fastapi import File
 from fastapi import UploadFile
+from fastapi.staticfiles import StaticFiles
 import aiofiles
+from PIL import Image
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
@@ -15,4 +18,8 @@ async def create_upload_file(file: UploadFile):
     async with aiofiles.open(file_location, 'wb') as out_file:
         content = await file.read()  # async read
         await out_file.write(content)  # async write
-    return {"filename": file.filename,"content/type":file.content_type,"message":"file saved"}
+    im = Image.open(file_location)
+    out = im.rotate(45) # degrees counter-clockwise
+    out.save("static/images/rot.png")
+    out.show()
+    return {"filename": file_location,"content/type":file.content_type,"message":"file saved"}
